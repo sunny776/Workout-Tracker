b54dde0078c1dbaeb28d008e9a761050a038b8c2
Sarness Prasad <sunny776@yahoo.com>
2021-05-21 13:32:55 -0700

    deploying

--- Develop/server.js
+++ Develop/server.js
@@ -1,22 +1,31 @@
 const mongoose = require("mongoose");
 const path = require("path");
-const routes = require("./routes")
+const routes = require("./routes");
 
 const express = require("express");
 const logger = require("morgan");
 
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true});
+//mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true});
 
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/deep-thoughts",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  }
+);
 const app = express();
 
 app.use(logger("dev"));
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 app.use(routes);
 
-app.listen(PORT, function() {
-    console.log(`Runnning on port ${PORT}`);
-})
+app.listen(PORT, function () {
+  console.log(`Runnning on port ${PORT}`);
+});
